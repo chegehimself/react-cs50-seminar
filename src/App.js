cc559a5c321a936a45f3753f1405d4c8cbf33c7f
james-chege <jymeschege@gmail.com>
2018-03-23 16:46:41 +0300

    add delete functionality

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import SingleTodo from "./SingleTodo";
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       todos: [],
       currentTodo: ""
     };
   }
-  // function to take in an event
+
   onInputChange = e => {
     this.setState({ currentTodo: e.target.value });
   }
@@ -19,26 +21,32 @@ class App extends Component {
     let todosCopy = this.state.todos.slice();
     todosCopy.push(this.state.currentTodo);
 
-    this.setState({ todos: todosCopy, currentTodo: ""});
+    this.setState({ todos: todosCopy, currentTodo: "" });
   }
 
-  render () {
-    // javascript map to take in an element and an index
+  deleteTodo = i => {
+    let todosCopy = this.state.todos.slice();
+
+    todosCopy.splice(i, 1);
+
+    this.setState({ todos: todosCopy });
+  }
+
+  render() {
     let bulletedTodos = this.state.todos.map((e, i) => {
       return (
-          <li key={i}>{e}</li>
-        );
+        <SingleTodo todo={e} delete={() => this.deleteTodo(i)}/>
+      );
     });
     return (
       <div>
-      <input placeholder="Enter todo" value={this.state.currentTodo} 
-      onChange={this.onInputChange}/>
-      <button onClick={this.onClick}>Add</button>
-      <br />
-
-      {this.state.todos.length === 0? "No todos yet!" : <ul>{bulletedTodos}</ul>}
+        <input placeholder="Enter todo" value={this.state.currentTodo}
+          onChange={this.onInputChange}/>
+        <button onClick={this.onClick}>Add!</button>
+        <br />
+        { this.state.todos.length === 0 ? "No todos yet!" : <ul>{bulletedTodos}</ul> }
       </div>
-      );
+    );
   }
 }
 
